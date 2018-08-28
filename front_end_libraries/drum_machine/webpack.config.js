02ef9e599e544adcd8e026a73891b66a8487bee8
Joel Balcaen <jobalcaen@gmail.com>
2018-08-28 13:45:16 -0500

    install modules, configure webpack and create app structure

---
+++ front_end_libraries/drum_machine/webpack.config.js
@@ -0,0 +1,52 @@
+let HTMLWebpackPlugin = require(
+	'html-webpack-plugin'
+);
+
+let HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+	template: __dirname + '/src/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+
+let FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+
+module.exports = {
+	entry: __dirname + '/src/index.js',
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader','css-loader','sass-loader']
+			},
+			{
+	            test: /\.(png|jp(e*)g|svg)$/,  
+	            use: [{
+	                loader: 'url-loader',
+	                options: { 
+	                    limit: 8000, // Convert images < 8kb to base64 strings
+	                    name: 'images/[hash]-[name].[ext]'
+	                } 
+	            }]
+	        }
+		]
+	},
+	output: {
+		filename: 'transformed.js',
+		path: __dirname + '/build'
+	},
+	plugins: [
+		HTMLWebpackPluginConfig,
+		// new FaviconsWebpackPlugin({logo: './app/images/headshot.jpg',
+		// 	prefix: 'favicons/',
+		// 	statsFilename: 'iconstats.json',
+		//     title: 'Joel Balcaen',
+		// })
+	]
+};
---
+++ responsive_web_design/survey_form/submit_button_disable.js
@@ -0,0 +1,6 @@
+
+window.onload = function() {
+	console.log('script loaded');
+	var name = document.querySelectorAll("input#name");
+
+};
