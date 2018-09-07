906840e213ddbff7bca7f5c67019edd2f090cb3d
Joel Balcaen <jobalcaen@gmail.com>
2018-09-07 18:57:24 -0400

    messing around with react-sound

--- front_end_libraries/drum_machine/src/components/DrumButton.js
+++ front_end_libraries/drum_machine/src/components/DrumButton.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DeleteDrum from './DeleteDrum';
+import Sound from 'react-sound';
 
-const DrumButton = (props) => (
-	<div className="drum-button">
-		{props.title}
-		<DeleteDrum id={props.id}/>
-		<audio
-        	id="t-rex-roar"
-        	controls
-        	src="http://soundbible.com/mp3/Tyrannosaurus%20Rex%20Roar-SoundBible.com-807702404.mp3">
-        	Your browser does not support the <code>audio</code> element.
-    	</audio>
-	</div>
-)
+
+class DrumButton extends Component {
+	constructor () {
+		super();
+		this.state = {
+			soundStatus: STOPPED
+		}
+	}
+
+	render () {
+		return (
+			<div className="drum-button">
+				{props.title}
+				<DeleteDrum id={props.id}/>
+		    	<Sound
+			      url="/src/drum_audio/acoustic_hat_02.wav"
+			      playStatus={Sound.status.PLAYING}
+			      onFinishedPlaying={console.log("sound finised")}
+			    />
+			</div>
+		)
+	}
+
+}
 
 export default DrumButton;
